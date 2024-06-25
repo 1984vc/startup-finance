2381efaa3e58aee124d77559a33a8aadfc23c25b
Mark Percival <m@mdp.im>
2024-06-25 22:21:55 +0000

    Next deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/safe-worksheet",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
