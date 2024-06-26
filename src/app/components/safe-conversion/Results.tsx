17cc4a7dc228c0b6b736d8d6afc4c639855dd577
Mark Percival <m@mdp.im>
2024-06-26 10:53:37 +0000

    Better Results

--- src/app/components/safe-conversion/Results.tsx
+++ src/app/components/safe-conversion/Results.tsx
@@ -1,6 +1,8 @@
 import { BestFit } from "@/library/safe_conversion";
 import { ConversionState, RowsProps } from "./Conversion";
 import { SAFEInputData } from "./SafeNoteList";
+import { CommonStockInputData } from "./CommonStockList";
+import { SeriesInputData } from "./SeriesInvestmentList";
 
 interface ResultProps {
   state: ConversionState;
@@ -8,6 +10,16 @@ interface ResultProps {
 }
 
 const Results: React.FC<ResultProps> = ({ state, bestFit }) => {
+  const commonShareholders = (
+    state.rowData.filter((r) => r.type === "common") as CommonStockInputData[]
+  ).map((r) => {
+    return {
+      name: r.name,
+      shares: r.shares,
+      ownership: r.shares / bestFit.totalShares,
+    };
+  });
+
   const convertedSafes = (
     state.rowData.filter((r) => r.type === "safe") as SAFEInputData[]
   ).map((r, idx) => {
@@ -21,16 +33,80 @@ const Results: React.FC<ResultProps> = ({ state, bestFit }) => {
     };
   });
 
+  const seriesShareholders = (
+    state.rowData.filter((r) => r.type === "series") as SeriesInputData[]
+  ).map((r) => {
+    const shares = r.investment / bestFit.pps;
+    return {
+      name: r.name,
+      shares,
+      ownership: shares / bestFit.totalShares,
+    };
+  });
+
   return (
     <div>
-      {convertedSafes.map((safe, index) => (
-        <div key={index}>
-          <p>Name: {safe.name}</p>
-          <p>Shares: {safe.shares}</p>
-          <p>Price per Share: {safe.pps}</p>
-          <p>Ownership: {safe.ownership * 100}%</p>
-        </div>
-      ))}
+      <h2 className="text-2xl font-bold mb-4">Results</h2>
+      <div className="space-y-4">
+        <h3 className="text-2xl font-bold mb-4">Common</h3>
+        {commonShareholders.map((common, index) => (
+          <div
+            key={index}
+            className="flex flex-row items-center space-x-4 bg-gray-100 p-4 rounded-lg"
+          >
+            <div className="flex-1">
+              <p className="font-semibold">Name: {common.name}</p>
+            </div>
+            <div className="flex-1">
+              <p>Shares: {common.shares.toFixed(2)}</p>
+            </div>
+            <div className="flex-1">
+              <p>Ownership: {(common.ownership * 100).toFixed(2)}%</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="space-y-4">
+        <h3 className="text-2xl font-bold mb-4">SAFE Investors</h3>
+        {convertedSafes.map((safe, index) => (
+          <div
+            key={index}
+            className="flex flex-row items-center space-x-4 bg-gray-100 p-4 rounded-lg"
+          >
+            <div className="flex-1">
+              <p className="font-semibold">Name: {safe.name}</p>
+            </div>
+            <div className="flex-1">
+              <p>Shares: {safe.shares.toFixed(2)}</p>
+            </div>
+            <div className="flex-1">
+              <p>Price per Share: ${safe.pps.toFixed(2)}</p>
+            </div>
+            <div className="flex-1">
+              <p>Ownership: {(safe.ownership * 100).toFixed(2)}%</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="space-y-4">
+        <h3 className="text-2xl font-bold mb-4">Series Investors</h3>
+        {seriesShareholders.map((investor, index) => (
+          <div
+            key={index}
+            className="flex flex-row items-center space-x-4 bg-gray-100 p-4 rounded-lg"
+          >
+            <div className="flex-1">
+              <p className="font-semibold">Name: {investor.name}</p>
+            </div>
+            <div className="flex-1">
+              <p>Shares: {investor.shares.toFixed(2)}</p>
+            </div>
+            <div className="flex-1">
+              <p>Ownership: {(investor.ownership * 100).toFixed(2)}%</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
