e19c422ff8da06615f3baaa8a04c778c20593d94
Mark Percival <m@mdp.im>
2024-08-01 15:30:40 +0000

    Add results test

---
+++ src/app/safe-conversion/state/__tests__/ResultSelector.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "@jest/globals";
+import { createConversionStore, IConversionStateData } from "../ConversionState";
+import { getResultsPropsSelector } from "../ResultSelector";
+import fixtureData from "./fixtures/state_fixtures.json"
+
+// Test our Result Selector, which handles both showing the resulting cap table and allow users to play around with the pre-money and investment changes
+describe("Result Selector", () => {
+  test("Basic sanity check of selector", () => {
+    const store = createConversionStore(fixtureData as IConversionStateData);
+    const resultSelector = getResultsPropsSelector({
+      ...store.getState(),
+      hasNewRound: true,
+      preMoneyChange: 0,
+      investmentChange: 0,
+    });
+    console.log(resultSelector.shareholders);
+    expect(resultSelector.shareholders[0].ownershipPct.toFixed(2)).toEqual("24.13");
+    expect(resultSelector.shareholders[0].ownershipChange).toEqual(0);
+  });
+  test("Changing investment amounts", () => {
+    const store = createConversionStore(fixtureData as IConversionStateData);
+    const resultSelector = getResultsPropsSelector({
+      ...store.getState(),
+      hasNewRound: true,
+      preMoneyChange: 0,
+      investmentChange: 500_000,
+    });
+    console.log(resultSelector.shareholders);
+    expect(resultSelector.shareholders[0].ownershipPct.toFixed(2)).toEqual("23.28");
+    expect(resultSelector.totalShares).toEqual(12_886_615);
+    expect(resultSelector.shareholders[0].ownershipChange.toFixed(2)).toEqual("-0.85");
+  });
+});
